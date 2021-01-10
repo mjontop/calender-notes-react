dab700ebc11bd3fefedadda0ce3e71942d353226
Manikant Jha <mjontop0602@gmail.com>
2021-01-11 00:55:14 +0530

    Added calender, added display pannel, added adding pannel , functionality to add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Calendar  from 'react-calendar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [value, onChange] = useState(new Date());
+	console.log(value.toLocaleString('default', { month: 'long' }));
+	return (
+		<>
+			<div className='row'>
+				<div className='col-12 col-md-6 p-4'>
+					<Calendar onChange={onChange} value={value} />
+				</div>
+				<div className='col-12 col-md-6 p-4'>
+					<div className='bg-success text-white text-center rounded'>{taskDisplay(value)}</div>
+				</div>
+			</div>
+			<div className='row'>
+				<div className='col-12 col-md-6 p-4'>{addTask(value)}</div>
+				<div className='col-12 col-md-6'>asdf</div>
+			</div>
+		</>
+	);
+};
+
+const taskDisplay = (date) => {
+	const heading = `${date.getDate()} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`;
+	return (
+		<>
+			<h2> {heading}</h2>
+		</>
+	);
+};
+
+const addTask = (date) => {
+	return (
+		<>
+			<div className='bg-lt-green p-5 rounded'>
+				<h2 className='text-light text-center rounded'>Add Task</h2>
+				<form>
+					<div class='form-group'>
+						<label>Title</label>
+						<input type='text' class='form-control' />
+					</div>
+					<div class='form-group'>
+						<label>Another label</label>
+						<textarea type='text' class='form-control' />
+					</div>
+				</form>
+				<button type='button' class='btn  btn-success btn-lg'>
+					Save
+				</button>
+				<button type='button' class='btn float-right btn-secondary btn-lg'>
+					Discard
+				</button>
+			</div>
+		</>
+	);
+};
 
 export default App;
